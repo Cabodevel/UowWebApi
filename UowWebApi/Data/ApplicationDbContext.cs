38f9ca4641b513f6b04312645e1a19f9c4738d4b
Carlos <carlosbolorino@hotmail.com>
2022-05-08 11:13:01 +0200

    Agregar archivos de proyecto.

---
+++ UowWebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UowWebApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
